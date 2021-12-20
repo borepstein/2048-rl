a00eb06ac3d5faefe0faae10da5a1eda514964d5
borepstein <borepstein@gmail.com>
2021-12-20 12:21:06 -0500

    Default model: relu: 16-512-512-512-512-4

--- py_2048_rl/models.py
+++ py_2048_rl/models.py
@@ -1,8 +1,23 @@
 import tensorflow as tf
 
-
 DEFAULT_MODEL = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(16, activation='tanh'),
+    tf.keras.layers.Dense(512, activation='tanh'),
+    tf.keras.layers.Dense(512, activation='tanh'),
+    tf.keras.layers.Dense(512, activation='tanh'),
+    tf.keras.layers.Dense(512, activation='tanh'),
+    tf.keras.layers.Dense(4, activation=None)
+])
+
+model_16_64_4_relu = tf.keras.models.Sequential([
     tf.keras.layers.Dense(16, activation='relu'),
     tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(4, activation=None)
+])
+
+model_16_256_256_4_relu = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(16, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
     tf.keras.layers.Dense(4, activation=None)
 ])
